c1d0db0cf015cdecdd334f18e0a572a827977913
SanjivSeeboruth <42058143+SanjivSeeboruth@users.noreply.github.com>
2018-08-03 21:10:33 +0400

    update

---
+++ Object Repository/Page_mysdworx - Aanmelden/button_Aanmelden.rs
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Aanmelden</name>
+   <tag></tag>
+   <elementGuidId>8279eba6-8231-4b4e-8c3e-0a432be169bb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;pwdform&quot;)/footer[@class=&quot;bottom&quot;]/button[@class=&quot;new-button equal&quot;][count(. | //button[@type = 'submit' and @class = 'new-button equal' and (text() = 'Aanmelden' or . = 'Aanmelden')]) = count(//button[@type = 'submit' and @class = 'new-button equal' and (text() = 'Aanmelden' or . = 'Aanmelden')])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>type</name>
+      <type>Main</type>
+      <value>submit</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>new-button equal</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Aanmelden</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pwdform&quot;)/footer[@class=&quot;bottom&quot;]/button[@class=&quot;new-button equal&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
