dfa9bcfec8e9c46db93964257e382dc08666478f
SanjivSeeboruth <42058143+SanjivSeeboruth@users.noreply.github.com>
2018-08-23 10:58:02 +0400

    update

--- Scripts/Sub Script/Navigation_Menu/Menu_Mes_rapports/Nouveau_Rapport/Mes rapports - SELECTION/Verify if checkbox is checked/TC 5 Verify Caractere juridique du contract checBox  is checked/Script1535007082942.groovy
+++ Scripts/Sub Script/Navigation_Menu/Menu_Mes_rapports/Nouveau_Rapport/Mes rapports - SELECTION/Verify if checkbox is checked/TC 5 Verify Caractere juridique du contract checBox  is checked/Script1535007082942.groovy
@@ -19,5 +19,5 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKe
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.check(findTestObject('Mes rapports Obj/SELECTION/Dossier and Groupe checkbox'))
+WebUI.check(findTestObject('Mes rapports Obj/SELECTION/Caractere juridique de contrat'))
 
